205230a61df7087b6ae7c6464d5e662b11ffd7e5
Lucas-FilpeSS <luskitta@gmail.com>
2020-04-02 02:27:05 -0300

    Exercícios

--- Ex 4/script.js
+++ Ex 4/script.js
@@ -1,9 +1,18 @@
 
-window.onload = function escrevaNumeros() {
-  // let ini = prompt('Digite o início');
-  // let fim = prompt('Digite o fim');
-
-
-
-
+window.onload = function init() {
+  let a = prompt('Número Inicial');
+  let b = prompt('Número Final');
+  let inicio = Number(a);
+  let fim = Number(b);
+  let cont = setTimeout(function escrevaNumeros(i, f) {
+    console.log(i);
+    i++;
+    if (i === f) {
+      setTimeout((i) => {
+        console.log(i);
+        clearTimeout(cont);
+      }, 1000, i);
+    }
+    cont = setTimeout(escrevaNumeros, 1000, i, f);
+  }, 1000, inicio, fim);
 };
--- Ex 5/script.js
+++ Ex 5/script.js
@@ -1,7 +1,33 @@
-let i = 0;
-setTimeout(() => alert(i), 100); // ?
-// Assuma que essa função demore mais que 100ms para executar
-for (let j = 0; j < 100000000; j++) {
-  i++;
-}
 
+window.onload = function init() {
+  let a = prompt('Número Inicial');
+  let b = prompt('Número Final');
+  let inicio = Number(a);
+  let fim = Number(b);
+
+  let cont = setTimeout(function escrevaNumeros(i, f) {
+    console.log(i);
+    i++;
+    if (i === f) {
+      setTimeout((i) => {
+        console.log(i);
+        clearTimeout(cont);
+      }, 1000, i);
+    }
+    cont = setTimeout(escrevaNumeros, 1000, i, f);
+  }, 1000, inicio, fim);
+
+
+  let interval = setInterval(function escrevaNumeros(i, f) {
+    console.log(i);
+    clearInterval(interval);
+    i++;
+    interval = setInterval(escrevaNumeros, 1000, i, f);
+    if (i === (f + 1)) {
+      clearInterval(interval);
+    }
+
+  }, 1000, inicio, fim);
+
+
+};
